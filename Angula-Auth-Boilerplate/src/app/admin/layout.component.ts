1feac607f4b45eeb9f6088e658fc2175631bd632
Lance Satorre <lanceerrotas@gmail.com>
2026-05-14 00:24:55 +0800

    Added Folders and Some Empty files

--- Angula-Auth-Boilerplate/src/app/admin/layout.component.ts
+++
@@ -1,4 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({ selector: 'app-admin-layout', templateUrl: 'layout.component.html', standalone: false })
-export class LayoutComponent { }
--- Angula-Auth-Boilerplate/src/app/admin/subnav.component.ts
+++
@@ -1,4 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({ selector: 'app-admin-subnav', templateUrl: 'subnav.component.html', standalone: false })
-export class SubNavComponent { }
