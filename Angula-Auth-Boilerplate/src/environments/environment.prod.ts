0bee9826d8b89ae233de639475413155be307458
Lance Satorre <lanceerrotas@gmail.com>
2026-05-20 17:18:41 +0800

    change apiURL

--- Angula-Auth-Boilerplate/src/environments/environment.prod.ts
+++ Angula-Auth-Boilerplate/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://deployment-backendintprog-production.up.railway.app',
+    apiUrl: 'https://abundant-eagerness-production.up.railway.app',
     useFakeBackend: false
 };
 
